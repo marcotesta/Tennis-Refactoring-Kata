37723a5956802245ebf176b93742597c0afca159
marco <mrc.tst@gmail.com>
2015-06-23 23:56:03 +0200

    Rearrange conditionals

--- java/src/main/java/Score.java
+++ java/src/main/java/Score.java
@@ -1,12 +1,19 @@
 
 public class Score {
 
+    public static final Score FORTY = new Score(3);
+
     private int value;
     private String playerName;
 
     public Score(String aPlayerName) {
         this.value = 0;
         this.playerName = aPlayerName;
+    }
+
+    private Score(int aValue) {
+        this.value = aValue;
+        this.playerName = "";
     }
 
     public boolean belongTo(String aPlayerName) {
@@ -21,12 +28,54 @@ public class Score {
         return this.value == that.value;
     }
 
+    public boolean greaterOrEqual(Score that) {
+        return this.value >= that.value;
+    }
+
+    public boolean greaterThan(Score that) {
+        return this.value > that.value;
+    }
+
+    public boolean lessThan(Score that) {
+        return this.value < that.value;
+    }
+
     public int getValue() {
         return value;
     }
 
     public String getPlayerName() {
         return playerName;
+    }
+
+    public int difference(Score that) {
+        return Math.abs(this.value - that.value);
+    }
+
+
+    public Score max(Score that) {
+        if (this.value >= that.value) {
+            return this;
+        } else {
+            return that;
+        }
+    }
+
+    @Override
+    public String toString() {
+        switch(value)
+        {
+            case 0:
+                 return "Love";
+            case 1:
+                 return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+            default:
+                return "";
+        }
     }
 
 }
--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -12,64 +12,33 @@ public class TennisGame1 implements TennisGame {
     public void wonPoint(String playerName) {
         if (player1Score.belongTo(playerName))
             player1Score.increment();
-
         else
             player2Score.increment();
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
         if (player1Score.even(player2Score)  )
         {
-            switch (player1Score.getValue())
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-
+            if (player1Score.lessThan(Score.FORTY)) {
+                score = player1Score.toString() + "-All";
+            } else if (player1Score.greaterOrEqual(Score.FORTY)) {
+                score = "Deuce";
             }
         }
-        else if (player1Score.getValue()>=4 || player2Score.getValue() >=4)
+        else if (player1Score.greaterThan(Score.FORTY) || player2Score.greaterThan(Score.FORTY))
         {
-            int minusResult = player1Score.getValue() - player2Score.getValue();
-            if (minusResult==1) score ="Advantage " + player1Score.getPlayerName();
-            else if (minusResult ==-1) score ="Advantage " + player2Score.getPlayerName();
-            else if (minusResult>=2) score = "Win for " + player1Score.getPlayerName();
-            else score ="Win for " + player2Score.getPlayerName();
+            if (player1Score.difference(player2Score) == 1) {
+                score ="Advantage " + player1Score.max(player2Score).getPlayerName();
+            } else if (player1Score.difference(player2Score) >= 2) {
+                score ="Win for " + player1Score.max(player2Score).getPlayerName();
+            }
         }
         else
         {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = player1Score.getValue();
-                else { score+="-"; tempScore = player2Score.getValue();}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+            score = player1Score.toString() + "-" + player2Score.toString();
         }
         return score;
     }
+
 }
